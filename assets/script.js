f479daeb1d27dd3b6da93cc3923bedd34de65681
Tristan Hanson <thanson1998@live.com>
2022-11-17 02:35:19 -0600

    quiz works

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,106 @@
-const quiz = document.querySelector("#quiz-questions")
-const start = document.getElementById('start')
+const quiz = document.querySelector("#quiz-questions");
+const start = document.getElementById('start');
+const title = document.getElementById('question-title');
+const answer1 = document.getElementById('ans1');
+const answer2 = document.getElementById('ans2');
+const answer3 = document.getElementById('ans3');
+const countdown = document.getElementById("time");
+const score = document.getElementById('score');
+const final = document.getElementById('final-score');
+const finalText = document.getElementById('final')
 
-start.textContent="start"
+var i = 0;
+var currScore = 0;
+var secondsLeft = 120;
+
+start.textContent="start";
+countdown.textContent = secondsLeft;
 
 const questions =[
     {
         title: "What does CSS stand for:",
         choices: ["Cascading Style Sheets", "Colorful soccer socks", "Caramel selling stand"],
-        answer: "Cascading Style Sheets"
     },
     {
         title: "What does HTML stand for?",
         choices: ["Hyper Text Markup Language", "How tall moths leap", "Heavy tank making links"],
-        answer: "Hyper Text Markup Language"
     },
     {
         title: "Is Java the same as JavaScript",
         choices: ["No", "Yes", "IDK Probably"],
-        answer: "No"
     },
     {
         title: "Who is the current president of Nepal",
         choices: ["Bidhya Devi Bhandari", "Pushpa Kamal Dahal", "Ram Baran Yadav"],
-        answer: "Bidhya Devi Bhandari"
     }
 ];
 
-var i = 0
-
 const createTitle = () =>{
-    let titleElem = document.createElement('h3')
-    titleElem.textContent=questions[i].title
-    quiz.append(titleElem)
-}
+    title.textContent=questions[i].title
+};
 
 const createQuestion = () =>{  
-    for (let x=0; x < questions[i].choices.length; x++){
-        var questionElem = document.createElement('button')
-        questionElem.textContent=questions[i].choices[x]
-        quiz.append(questionElem)
-        console.log(x)
-    }
-}
+    answer1.textContent=questions[i].choices[0]
+    answer2.textContent=questions[i].choices[1]
+    answer3.textContent=questions[i].choices[2]
+};
 
-start.addEventListener('click', ()=>{
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        countdown.textContent = secondsLeft;
+    }, 1000);
+};
+
+const newScore = () =>{
+    currScore++
+    score.textContent = currScore
+    final.textContent=currScore
+};
+
+const next = () =>{
     if(i < questions.length){
-        start.textContent="next"
         createTitle()
         createQuestion()
         i++
+        alert('Incorrect!')
+        secondsLeft -30
+    }else{
+        destroy()
     }
-})
+};
+
+const destroy = () =>{
+    quiz.remove()
+    finalText.style.visibility="visible"
+}
+
+
+start.addEventListener('click', ()=>{
+    createTitle()
+    createQuestion()
+    setTime()
+    i++
+    start.remove()
+});
+
+answer1.addEventListener('click', ()=>{
+    if(i < questions.length){
+        createTitle()
+        createQuestion()
+        newScore()
+        i++
+        alert('Correct!')
+    }else{
+        newScore()
+        destroy()
+    }
+});
+
+answer2.addEventListener('click', ()=>{
+    next()
+});
+
+answer3.addEventListener('click', ()=>{
+    next()
+});
